9dcf792002cb6d1bf410b232099c4b9d7e171a72
Sergey <aspirin_1988@mail.ru>
2016-04-22 16:04:06 +0600

    first edit to day

--- wp-content/themes/fest/functions.php
+++ wp-content/themes/fest/functions.php
@@ -150,3 +150,11 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_filter('single_template', create_function(
+		'$the_template',
+		'foreach( (array) get_the_category() as $cat ) {
+    if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+    return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+  return $the_template;' )
+);
