afd6b88d1014106379f6258bb566f4ae8f5b1884
Sergey <aspirin_1988@mail.ru>
2016-04-25 16:07:47 +0600

    first edit to day

--- wp-content/plugins/group-registration/group-registration.php
+++ wp-content/plugins/group-registration/group-registration.php
@@ -56,6 +56,36 @@ function reg_param()
 
 function get_all_group()
 {
+
+    if (isset($_POST['download']))
+    {
+        header("Content-Type: application/force-download");
+        header('Content-Type: application/octet-stream');
+        header('Content-Type: application/download');
+        header('Content-Disposition: attachment;filename=list.xls');
+        header('Content-Transfer-Encoding: binary');
+
+        xlsBOF(); //начинаем собирать файл
+        /*первая строка*/
+        xlsWriteLabel(1,0,'Название');
+        /*вторая строка*/
+        xlsWriteLabel(2,0,'№п/п');
+        xlsWriteLabel(2,1,'Имя');
+        xlsWriteLabel(2,2,'Фамилия');
+        /*третья строка*/
+        xlsWriteNumber(3,0,'1');
+        xlsWriteLabel(3,1,'Петр');
+        xlsWriteLabel(3,2,'Иванов');
+        /*...*/
+        xlsWriteNumber(32,0,'30');
+        xlsWriteLabel(32,1,'Иван');
+        xlsWriteLabel(32,2,'Петров');
+
+        xlsEOF(); //заканчиваем собирать
+        readfile('list.xls');
+        exit();
+    }
+
     if (!isset($_POST['show_data'])) {
         echo '<div class="fields_header"><div class="header" ><h1>Список разрешенных групп</h1> </div>';
     }
@@ -251,4 +281,27 @@ function get_main1()
     echo  $template;
 }
 
+
+function xlsBOF() {
+    echo pack(«ssssss», 0x809, 0x8, 0x0, 0x10, 0x0, 0x0);
+    return;
+}
+
+function xlsEOF() {
+    echo pack(«ss», 0x0A, 0x00);
+    return;
+}
+
+function xlsWriteNumber($Row, $Col, $Value) {
+    echo pack(«sssss», 0x203, 14, $Row, $Col, 0x0);
+    echo pack(«d», $Value);
+    return;
+}
+
+function xlsWriteLabel($Row, $Col, $Value ) {
+    $L = strlen($Value);
+    echo pack(«ssssss», 0x204, 8 + $L, $Row, $Col, 0x0, $L);
+    echo $Value;
+    return;
+}
 
